40d4b49868d6c75831f71524f1b78ed2ca56d410
diarmov <56406028+diarmov@users.noreply.github.com>
2023-09-26 08:55:19 -0600

    Update TablaEstadosPopup.jsx

--- src/components/tablero/TablaEstadosPopup.jsx
+++ src/components/tablero/TablaEstadosPopup.jsx
@@ -4,55 +4,62 @@ import { HiFolderOpen, HiOutlineServer } from 'react-icons/hi';
 import { useTableroStore } from '../../hooks/useTableroStore';
 
 export default function TablaEstadosPopup() {
-    const { denunciasEstatus, statusTotales } = useTableroStore()
-    return (
-      <>
-        <div className='flex justify-around'>
-          <div className='text-center'>
-            <div className='flex justify-center'>
-              <div className='p-3 bg-red-900 block w-12 h-12 rounded-lg text-center text-white text-1xl shadow-lg font-bold'>
-                OIC
-              </div>
+  const { denunciasEstatus, statusTotales } = useTableroStore()
+  console.log(denunciasEstatus);
+  return (
+    <>
+      <div className='flex justify-around'>
+        <div className='text-center'>
+          <div className='flex justify-center'>
+            <div className='block w-12 h-12 p-3 font-bold text-center text-white bg-red-900 rounded-lg shadow-lg text-1xl'>
+              OIC
             </div>
-            <p className='mt-3 font-semibold text-gray-700 text-center'>{statusTotales[0]?.oic}</p>
           </div>
-
-          <div className='text-center'>
-            <div className='flex justify-center'>
-              <div className='p-3 bg-red-900 block w-12 h-12 rounded-lg text-center text-white text-1xl shadow-lg font-bold'>
-                UIF
-              </div>
-            </div>
-            <p className='mt-3 font-semibold text-gray-700 text-center'>{statusTotales[0]?.uif}</p>
-          </div>
+          <p className='mt-3 font-semibold text-center text-gray-700'>{statusTotales[0]?.oic}</p>
         </div>
 
-        <Table hoverable>
+        <div className='text-center'>
+          <div className='flex justify-center'>
+            <div className='block w-12 h-12 p-3 font-bold text-center text-white bg-red-900 rounded-lg shadow-lg text-1xl'>
+              UIF
+            </div>
+          </div>
+          <p className='mt-3 font-semibold text-center text-gray-700'>{statusTotales[0]?.uif}</p>
+        </div>
+      </div>
+
+      <Table hoverable>
         <Table.Head>
           <Table.HeadCell>
             Estatus
           </Table.HeadCell>
-          <Table.HeadCell className='whitespace-nowrap text-center'>
-            Total de denuncias
-          </Table.HeadCell>         
+          <Table.HeadCell className='text-center whitespace-nowrap'>
+            Total de denuncias OIC
+          </Table.HeadCell>
+          <Table.HeadCell className='text-center whitespace-nowrap'>
+            Total de denuncias UIF
+          </Table.HeadCell>
         </Table.Head>
         <Table.Body className="divide-y">
-        {
-              denunciasEstatus.map((denuncia, index) => (
-                <Table.Row className="bg-white" key={ index }>                           
-                  
-                  <Table.Cell className="whitespace-nowrap">                       
-                       { denuncia.estatus }
-                  </Table.Cell>
+          {
+            denunciasEstatus.map((denuncia, index) => (
+              <Table.Row className="bg-white" key={index}>
 
-                  <Table.Cell className='whitespace-nowrap text-center'>
-                       { denuncia.total }
-                  </Table.Cell>
-                </Table.Row>
+                <Table.Cell className="whitespace-nowrap">
+                  {denuncia.estatus}
+                </Table.Cell>
+
+                <Table.Cell className='text-center whitespace-nowrap'>
+                  {denuncia.oic}
+                </Table.Cell>
+                <Table.Cell className='text-center whitespace-nowrap'>
+                  {denuncia.uif}
+                </Table.Cell>
+              </Table.Row>
             ))
-        }
+          }
         </Table.Body>
       </Table>
-      </>
-    )
+    </>
+  )
 }

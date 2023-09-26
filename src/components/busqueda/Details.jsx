9e23afaadabaa1b7252e397d6543a3077c255b30
diarmov <56406028+diarmov@users.noreply.github.com>
2023-09-26 11:14:47 -0600

    conteo por ubicación

--- src/components/busqueda/Details.jsx
+++ src/components/busqueda/Details.jsx
@@ -4,48 +4,56 @@ import { useBusquedaStore } from '../../hooks/useBusquedaStore';
 export const Details = () => {
   const { details } = useBusquedaStore()
   return (
-    <div className='flex justify-around flex-wrap my-10'>
-      <div className='text-center mb-4'>
+    <div className='flex flex-wrap justify-around my-10'>
+      <div className='mb-4 text-center'>
         <div className='flex justify-center'>
-         <div className='p-3 bg-red-900 block w-12 h-12 rounded-lg text-center text-white text-2xl shadow-lg'>
+          <div className='block w-12 h-12 p-3 text-2xl text-center text-white bg-red-900 rounded-lg shadow-lg'>
             <HiFolderOpen />
           </div>
         </div>
 
         <p className='mt-3 font-semibold text-gray-700 '>Detalle de denuncias</p>
         <div className='mt-3 text-sm'>
-            <p>Numero de denuncias a la fecha: <span className='font-semibold'>{ details.denuncias }</span></p>
-            <p>En atención por los OIC: <span className='font-semibold'>{ details.oics }</span></p>
-            <p>En atención por la SFP: <span className='font-semibold'>{ details.sfp }</span></p>
-            <p>Por falta grave: <span className='font-semibold'>{ details.grave }</span></p>
-            <p>Por falta no grave: <span className='font-semibold'>{ details.noGrave }</span></p>
-            <p>Expedientes con ambos tipos de falta: <span className='font-semibold'>{ details.ambos }</span></p>
+          <p>Numero de denuncias a la fecha: <span className='font-semibold'>{details.denuncias}</span></p> <br />
+          <span className='font-bold'>En atención por: </span><br />
+          <hr />
+          <p>OIC: <span className='font-semibold'>{details.oics}  </span>
+            UIF: <span className='font-semibold'>{details.sfp}  </span>
+            DR: <span className='font-semibold'>{details.dr}  </span>
+            DRSP: <span className='font-semibold'>{details.drsp}</span></p>
+          <p>TJA: <span className='font-semibold'>{details.tja}  </span>
+            JFC: <span className='font-semibold'>{details.jfc}  </span>
+            DC: <span className='font-semibold'>{details.dc}</span></p><br />
+          <hr />
+          <p>Por falta grave: <span className='font-semibold'>{details.grave}</span></p>
+          <p>Por falta no grave: <span className='font-semibold'>{details.noGrave}</span></p>
+          <p>Expedientes con ambos tipos de falta: <span className='font-semibold'>{details.ambos}</span></p>
         </div>
       </div>
 
 
-       <div className='text-center'>
+      <div className='text-center'>
         <div className='flex justify-center'>
-         <div className='p-3 bg-red-900 block w-12 h-12 rounded-lg text-center text-white text-2xl shadow-lg'>
+          <div className='block w-12 h-12 p-3 text-2xl text-center text-white bg-red-900 rounded-lg shadow-lg'>
             <HiOutlineServer />
           </div>
         </div>
 
-        <p className='mt-3 font-semibold text-gray-700 text-center'>Por Etapa</p>
-            <div className='mt-3 text-sm grid grid-cols-2 text-left'>               
-               <div>Investigación: <span className='font-semibold ml-2'>{ details.investigacion }</span></div>
-               <div>Substanciación: <span className='font-semibold ml-2'>{ details.substanciacion }</span> </div>
-               <div>En Resolución: <span className='font-semibold ml-2'>{ details.resolucion }</span></div>
-               <div>Resolución o Sentencia: <span className='font-semibold ml-2'>{ details.resSent }</span></div>
-               <div>Impugnación: <span className='font-semibold ml-2'>{ details.impugnacion }</span></div>
-               <div>Amparo: <span className='font-semibold ml-2'>{ details.amparo }</span></div>
-               <div>Firme: <span className='font-semibold ml-2'>{ details.firme }</span></div>
-               <div>Conclusión y Archivo: <span className='font-semibold ml-2'>{ details.conArch }</span></div>
-               <div>Inprocedentes: <span className='font-semibold ml-2'>{ details.inprocedentes }</span></div>
-               <div>Sobreseimiento: <span className='font-semibold ml-2'>{ details.Sobreseimiento }</span></div>
-               <div>En Proceso: <span className='font-semibold ml-2'>{ details.proceso }</span></div>
-            </div>
+        <p className='mt-3 font-semibold text-center text-gray-700'>Por Etapa</p>
+        <div className='grid grid-cols-2 mt-3 text-sm text-left'>
+          <div>Investigación: <span className='ml-2 font-semibold'>{details.investigacion}</span></div>
+          <div>Substanciación: <span className='ml-2 font-semibold'>{details.substanciacion}</span> </div>
+          <div>En Resolución: <span className='ml-2 font-semibold'>{details.resolucion}</span></div>
+          <div>Resolución o Sentencia: <span className='ml-2 font-semibold'>{details.resSent}</span></div>
+          <div>Impugnación: <span className='ml-2 font-semibold'>{details.impugnacion}</span></div>
+          <div>Amparo: <span className='ml-2 font-semibold'>{details.amparo}</span></div>
+          <div>Firme: <span className='ml-2 font-semibold'>{details.firme}</span></div>
+          <div>Conclusión y Archivo: <span className='ml-2 font-semibold'>{details.conArch}</span></div>
+          <div>Inprocedentes: <span className='ml-2 font-semibold'>{details.inprocedentes}</span></div>
+          <div>Sobreseimiento: <span className='ml-2 font-semibold'>{details.Sobreseimiento}</span></div>
+          <div>En Proceso: <span className='ml-2 font-semibold'>{details.proceso}</span></div>
         </div>
+      </div>
 
     </div>
   )
--- src/helpers/fncBusqueda.js
+++ src/helpers/fncBusqueda.js
@@ -1,45 +1,50 @@
 export const fncBusqueda = () => {
-  
-   const onDetails = ( denuncias ) => {
-     return {
-        denuncias: denuncias.length,
-        oics: (denuncias.filter(d => d.idUbicacion === 1)).length,
-        sfp: (denuncias.filter(d => d.idUbicacion !== 1)).length,
-        grave: (denuncias.filter(d => d.idTipoFalta === 1)).length,
-        noGrave: (denuncias.filter(d => d.idTipoFalta === 2)).length,
-        ambos: (denuncias.filter(d => d.idTipoFalta === 3)).length,
-        investigacion: (denuncias.filter(d => d.idEtapa === 1)).length,
-        substanciacion: (denuncias.filter(d => d.idEtapa === 2)).length,
-        resolucion: (denuncias.filter(d => d.idEtapa === 3)).length,
-        resSent: (denuncias.filter(d => d.idEtapa === 4)).length,
-        impugnacion: (denuncias.filter(d => d.idEtapa === 5)).length,
-        amparo: (denuncias.filter(d => d.idEtapa === 6)).length,
-        firme: (denuncias.filter(d => d.idEtapa === 7)).length,
-        conArch: (denuncias.filter(d => d.idEtapa === 8)).length,
-        inprocedentes: (denuncias.filter(d => d.idEtapa === 9)).length,
-        Sobreseimiento: (denuncias.filter(d => d.idEtapa === 10)).length,
-        proceso: (denuncias.filter(d => d.idEtapa === 11)).length
-     }
+
+   const onDetails = (denuncias) => {
+      return {
+         denuncias: denuncias.length,
+         oics: (denuncias.filter(d => d.idUbicacion === 1)).length,
+         sfp: (denuncias.filter(d => d.idUbicacion === 2)).length,
+         dr: (denuncias.filter(d => d.idUbicacion === 3)).length,
+         drsp: (denuncias.filter(d => d.idUbicacion === 4)).length,
+         tja: (denuncias.filter(d => d.idUbicacion === 5)).length,
+         jfc: (denuncias.filter(d => d.idUbicacion === 6)).length,
+         dc: (denuncias.filter(d => d.idUbicacion === 7)).length,
+         grave: (denuncias.filter(d => d.idTipoFalta === 1)).length,
+         noGrave: (denuncias.filter(d => d.idTipoFalta === 2)).length,
+         ambos: (denuncias.filter(d => d.idTipoFalta === 3)).length,
+         investigacion: (denuncias.filter(d => d.idEtapa === 1)).length,
+         substanciacion: (denuncias.filter(d => d.idEtapa === 2)).length,
+         resolucion: (denuncias.filter(d => d.idEtapa === 3)).length,
+         resSent: (denuncias.filter(d => d.idEtapa === 4)).length,
+         impugnacion: (denuncias.filter(d => d.idEtapa === 5)).length,
+         amparo: (denuncias.filter(d => d.idEtapa === 6)).length,
+         firme: (denuncias.filter(d => d.idEtapa === 7)).length,
+         conArch: (denuncias.filter(d => d.idEtapa === 8)).length,
+         inprocedentes: (denuncias.filter(d => d.idEtapa === 9)).length,
+         Sobreseimiento: (denuncias.filter(d => d.idEtapa === 10)).length,
+         proceso: (denuncias.filter(d => d.idEtapa === 11)).length
+      }
 
    }
 
-   const onGetData = ( tipo ) => {
-      var color = ['green','#7E212C','skyblue','purple','pink','green','orange','aqua','red']
+   const onGetData = (tipo) => {
+      var color = ['green', '#7E212C', 'skyblue', 'purple', 'pink', 'green', 'orange', 'aqua', 'red']
       let data = []
       let labels = []
 
       tipo.map(item => {
-         data.push( item.total )
-         labels.push( item.nombre )
+         data.push(item.total)
+         labels.push(item.nombre)
       })
 
       return {
          data,
          labels,
-         color 
+         color
       }
    }
-  
+
    return { onDetails, onGetData }
 }
 
